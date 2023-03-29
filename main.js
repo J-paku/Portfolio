896fe9792b9265e55b666cf81121fccb90aedb5e
Park JaeHong <pjhrecr@gmail.com>
2023-03-29 21:22:57 +0900

    JS:Menu Toogle

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -12,6 +13,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 //  Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -20,16 +22,24 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null){
         return;
     }
-
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toogle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__button');
 homeContactBtn.addEventListener('click', () => {
-    
     scrollIntoView('#contact');
 });
+
 
 // Handle the TopPage Button
 const topPageButton = document.querySelector('.topPage');
@@ -39,14 +49,13 @@ topPageButton.addEventListener('click', () => {
 });
 
 
-
-
 // Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=> {
     home.style.opacity = 0.9 - window.scrollY / homeHeight;
 }); 
+
 
 // Show "Page up" button when scrolling down
 const arrowUp = document.querySelector('.topPage');
@@ -58,6 +67,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 // Projects
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
@@ -68,6 +78,7 @@ workBtnContainer.addEventListener('click', (ev) =>{
         return;
     }
 
+
     // Remove selection from the previous item and select the new one
     const active = document.querySelector('.category__btn.selected');
     active.classList.remove('selected');
@@ -75,8 +86,8 @@ workBtnContainer.addEventListener('click', (ev) =>{
         ev.target.nodeName === 'BUTTON' ? ev.target : ev.target.parentNode;
     target.classList.add('selected');
 
-
     projectContainer.classList.add('anime-out');
+
 
     //for(let project of projects), for(let i=0; i< projects.lengthi++){project = projects[i];と等しい}
     projects.forEach((project) => {
@@ -90,7 +101,6 @@ workBtnContainer.addEventListener('click', (ev) =>{
         projectContainer.classList.remove('anime-out');
     }, 300);
 });
-
 
 
 function scrollIntoView(selector) {
