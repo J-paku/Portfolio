6b400286100a1be40f5aa6000b061765ccbc3113
Park JaeHong <pjhrecr@gmail.com>
2023-05-10 22:55:02 +0900

    css fix2

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ topPageButton.addEventListener('click', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=> {
-    home.style.opacity = 0.9 - window.scrollY / homeHeight;
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 }); 
 
 
